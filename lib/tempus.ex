aee4dc84aabf3633d21205a32bfe11a7f439f9ae
Joel Carlbark <joel.carlbark@meltwater.com>
2019-06-04 11:21:53 +0200

    Add property based test

--- lib/tempus.ex
+++ lib/tempus.ex
@@ -3,7 +3,9 @@ defmodule Tempus do
   Documentation for Tempus.
   """
 
-  def shift(datetime, days: n) do
+  def shift(datetime, shift_opts, resolver_fn \\ &default_resolver/1)
+
+  def shift(datetime, [days: n], resolver_fn) do
     erl_date =
       datetime
       |> DateTime.to_date()
@@ -15,26 +17,36 @@ defmodule Tempus do
     utc_erl_time = DateTime.to_time(datetime) |> Time.to_erl()
     {:ok, naive} = NaiveDateTime.from_erl({shifted_erl_date, utc_erl_time})
 
-    DateTime.from_naive(naive, datetime.time_zone, Tzdata.TimeZoneDatabase)
+    case DateTime.from_naive(naive, datetime.time_zone, Tzdata.TimeZoneDatabase) do
+      {:ok, _} = ok ->
+        ok
+      other ->
+        {:ok, resolver_fn.(other)}
+    end
   end
 
-  def shift(datetime, months: 0) do
+  def shift(datetime, [months: 0], _) do
     {:ok, datetime}
   end
 
-  def shift({:ok, datetime}, months: 1), do: shift(datetime, months: 1)
+  def shift({:ok, datetime}, [months: 1], resolver_fn), do: shift(datetime, [months: 1], resolver_fn)
+  def shift({:ambiguous, _, _} = amb, [months: 1], resolver_fn), do: amb |> resolver_fn.() |> shift([months: 1], resolver_fn)
+  def shift({:gap, _, _} = gap, [months: 1], resolver_fn), do: gap |> resolver_fn.() |> shift([months: 1], resolver_fn)
 
-  def shift(datetime, months: 1) do
+  def shift(datetime, [months: 1], resolver_fn) do
     days_in_month = Calendar.ISO.days_in_month(datetime.year, datetime.month)
     remaining_days_in_month = days_in_month - datetime.day
-    shift(datetime, days: remaining_days_in_month + 1)
+    shift(datetime, [days: remaining_days_in_month + 1], resolver_fn)
   end
 
-  def shift({:ok, datetime}, months: n) when n > 1, do: shift(datetime, months: n)
+  def shift({:ok, datetime}, [months: n], resolver_fn) when n > 1, do: shift(datetime, [months: n], resolver_fn)
 
-  def shift(datetime, months: n) when n > 1 do
+  def shift(datetime, [months: n], resolver_fn) when n > 1 do
     datetime
-    |> shift(months: 1)
-    |> shift(months: n - 1)
+    |> shift([months: 1], resolver_fn)
+    |> shift([months: n - 1], resolver_fn)
   end
+
+  defp default_resolver({:ambiguous, _first, second}), do: second
+  defp default_resolver({:gap, _just_before, just_after}), do: just_after
 end
--- test/tempus_test.exs
+++ test/tempus_test.exs
@@ -1,15 +1,58 @@
 defmodule TempusTest do
   use ExUnit.Case
+  use ExUnitProperties
   doctest Tempus
 
-  test "shift" do
-    date = DateTime.from_naive!(~N[2019-04-30T21:30:00], "Etc/UTC")
-    time_zone = "Europe/Helsinki"
-    {:ok, date_with_timezone} = DateTime.shift_zone(date, time_zone, Tzdata.TimeZoneDatabase)
+  describe "shift" do
+    test "helsinki 1 month" do
+      date = DateTime.from_naive!(~N[2019-04-30T21:30:00], "Etc/UTC")
+      time_zone = "Europe/Helsinki"
+      {:ok, date_with_timezone} = DateTime.shift_zone(date, time_zone, Tzdata.TimeZoneDatabase)
 
-    {:ok, shifted_date} = Tempus.shift(date_with_timezone, months: 1)
+      {:ok, shifted_date} = Tempus.shift(date_with_timezone, months: 1)
 
-    assert DateTime.to_iso8601(shifted_date) == "2019-06-01T00:30:00+03:00"
-    assert shifted_date.time_zone == time_zone
+      assert DateTime.to_iso8601(shifted_date) == "2019-06-01T00:30:00+03:00"
+      assert shifted_date.time_zone == time_zone
+    end
+
+    property "is always in the future" do
+      check all shift_unit <- StreamData.member_of([:days, :months]),
+                time_zone <- time_zone_generator(),
+                shift_amount <- StreamData.positive_integer(),
+                now <- date_time_generator(),
+                max_runs: 1000 do
+        {:ok, now_with_tz} = DateTime.shift_zone(now, time_zone, Tzdata.TimeZoneDatabase) |> unambiguate()
+
+        {:ok, shifted} = Tempus.shift(now_with_tz, [{shift_unit, shift_amount}])
+
+        assert DateTime.compare(shifted, now_with_tz) == :gt
+      end
+    end
+  end
+
+  defp unambiguate({:ok, datetime}), do: {:ok, datetime}
+  defp unambiguate({:ambiguous, first, _second}), do: {:ok, first}
+
+  defp date_time_generator() do
+    [
+      StreamData.integer(2018..2030),
+      StreamData.integer(1..12),
+      StreamData.integer(1..28),
+      StreamData.integer(0..23),
+      StreamData.integer(0..59),
+      StreamData.integer(0..59)
+    ]
+    |> StreamData.fixed_list()
+    |> StreamData.map(fn [year, month, day, hour, minute, second] ->
+      with {:ok, naive} <- NaiveDateTime.from_erl({{year, month, day}, {hour, minute, second}}),
+           {:ok, datetime} <- DateTime.from_naive(naive, "Etc/UTC") do
+        datetime
+      end
+    end)
+  end
+
+  defp time_zone_generator() do
+    Tzdata.zone_list()
+    |> StreamData.member_of()
   end
 end
